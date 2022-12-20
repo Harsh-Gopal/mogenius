3740a4e9765777ad1eb0351348eadd3f6185a81d
RituRajPS <theriturajps@gmail.com>
2022-12-20 19:01:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ app = Flask(__name__)
 def hello_world():
    return 'Hello, World!'
 
-#Ex https://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
-os.system("git clone https://username:token@github.com/username/reponame ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 main.py &")
+#Ex https://theriturajps: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@github.com/theriturajps/File-Sharing
+os.system("git clone https://theriturajps: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@github.com/theriturajps/File-Sharing ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 main.py &")
