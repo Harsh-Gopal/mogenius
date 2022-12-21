ad3a8b5bda205d4f5274ca40702c5f5a612512d3
Harsh Gopal <106465476+HarshGopalHG@users.noreply.github.com>
2022-12-21 19:14:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'Hello, World!'
 
 #Ex https://theriturajps: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@github.com/theriturajps/File-Sharing
-os.system("git clone https://theriturajps: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@github.com/theriturajps/telegram-thumbnail ok && cd ok && pip3 install -U -r requirements.txt && nohup python -m renamer &")
+os.system("git clone https://theriturajps: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@github.com/theriturajps/telegram-thumbnail ok && cd ok && pip3 install -U -r requirements.txt && nohup python-m renamer &")
